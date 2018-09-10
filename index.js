2d81fbc846faf5217aa1cb39875593c7d49e7023
Heather McLeod <heather.mcleod@mail.utoronto.ca>
2018-09-10 14:06:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function currentLine (katzDeliLine) {
   if (katzDeliLine.length === 0){
     return "The line is currently empty."
   }
-var string = `The line is currently: 1.${katzDeliLine [0]}
+var string = `The line is currently: 1.${katzDeliLine [0]}`
 for (var i =1; i<katzDeliLine.length; i++){
  string+=`, ${i+1}. ${katzDeliLine[i]}`
 }
