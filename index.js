3166304f52eef8f7530d00c25fabb6c1c96e7c61
Heather McLeod <heather.mcleod@mail.utoronto.ca>
2018-09-10 14:04:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,14 @@ function nowServing (katzDeliLine) {
   else { return "Currently serving " + katzDeliLine.shift(katzDeliLine[0])+"."
 }
 }
+
+function currentLine (katzDeliLine) {
+  if (katzDeliLine.length === 0){
+    return "The line is currently empty."
+  }
+var string = "The line is currently: 1. ${katzDeliLine[0]}"
+for (var i =1; i<katzDeliLine.length; i++){
+ string+=`, ${i+1}. ${katzDeliLine[i]}`
+}
+return string
+}
